55c3c84f2e8d4a59296e96e7ed9a357e24244920
aeconomy7 <aeconomy7@gmail.com>
2020-09-08 12:45:02 +0000

    Added massive logic update

--- MovieNightBot/MovieNightBot.py
+++ MovieNightBot/MovieNightBot.py
@@ -8,6 +8,8 @@ from datetime import datetime, timedelta
 import urllib.parse
 import requests
 
+import emoji
+
 import asyncio
 import aiocron
 import aiohttp
@@ -15,6 +17,7 @@ import json
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 BOT_PREFIX=("!moviebot ","/moviebot ","!movie ","/movie ")
 TOKEN='XXXX'
@@ -78,6 +81,8 @@ async def vote_cmd(context,*movie_args):
 	# get arguements passed
 	movie_name = ' '.join(movie_args)
 
+	changed_vote = ""
+
 	if movie_name is '':
 		await context.send("Please supply a movie name :)")
 		return
@@ -118,7 +123,8 @@ async def vote_cmd(context,*movie_args):
 		user = fs[0]
 		movie = fs[1].strip("\n")
 		if user == str(context.message.author):
-			print("[!] " + str(context.message.author) + " changing vote: '" + movie.replace("_"," ") + "' -> '" + movie_name + "'")
+			changed_vote = movie.replace("_"," ")
+			print("[!] " + str(context.message.author) + " changing vote: '" + changed_vote + "' -> '" + movie_name + "'")
 		else:
 			vote_rewrite.write(line)
 	vote_rewrite.close()
@@ -126,8 +132,13 @@ async def vote_cmd(context,*movie_args):
 	# Add movie vote and discord name to file
 	with open("movie_votes.txt","a") as f:
 		f.write(str(context.message.author) + "///" + movie_name.replace(" ","_") + "\n")
-		await context.send("Successfully cast your movie vote for '" + movie_name + "'!")
+		if not (changed_vote == movie_name):
+			await context.send("Successfully changed your vote from **" + changed_vote + "** to **" + movie_name + "**")
+		else:
+			await context.send("Successfully cast your movie vote for **" + movie_name + "**!")
 		print("[+] Successfully cast new movie vote: " + str(context.message.author) + ":" + movie_name.replace(" ","_"))
+
+	return
 #~~~~~~~~~~~#
 # End /vote #
 #~~~~~~~~~~~#
@@ -160,16 +171,24 @@ async def status_cmd(context):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # NOTE: Make sure to enter channel / role ID in proper fields below
 #@bot.event
-#@aiocron.crontab('0 7 * * sun')
-@aiocron.crontab('* * * * *')
+@aiocron.crontab('0 7 * * sun')
+#@aiocron.crontab('*/2 * * * *')
 async def select_movie():
 #	channel_id = 751847219518242935 # Enter channel ID here (will be the channel where movies are announced)
-	channel_id = 729104553638625310 # Test channel
+	channel_id = 752590414699167814 # Test channel
 #	role_id    = 751833828729028729 # Enter role ID here (will be mentioned when movie is picked)
-	role_id    = 723181617090396261 # Test role
+	role_id    = 723296898852716667 # Test role
+
+	mins_to_movie = 1 # Number of minutes between random movie selection and movie time
 
 	vote_no = 0
 	all_votes = []
+
+	react_emojis = [
+		'1\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}',
+		'2\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}',
+		'3\N{VARIATION SELECTOR-16}\N{COMBINING ENCLOSING KEYCAP}'
+	]
 
 	msg = ""
 
@@ -188,9 +207,9 @@ async def select_movie():
 
 			all_votes.append(vote)
 
-			print("Loaded vote: " + str(vote))
+			print("[+] Loaded vote from 'movie_votes.txt': " + str(vote))
 
-	print("Total number of votes: " + str(len(all_votes)))
+	print("[?] Total number of votes: " + str(len(all_votes)))
 
 	# Main logic
 	if len(all_votes) == 0:
@@ -198,34 +217,59 @@ async def select_movie():
 		await channel.send(msg)
 		return
 	elif len(all_votes) == 1:
-		msg = msg + "There is only **1** vote this week!  The chosen movie is: `" + all_votes[0]['movie_name'] + "` (vote cast by `" + all_votes[0]['discord_name'] + "`)"
+		msg = msg + "<@&" + str(role_id) + ">'s!!!  There is only **1** vote this week!  The chosen movie is: `" + all_votes[0]['movie_name'].replace("_"," ") + "` (vote cast by `" + all_votes[0]['discord_name'] + "`)"
 		await channel.send(msg)
 		return
 	else:
-		print("range of random.sample: " + str(range(0,len(all_votes))))
+		# Debug
+		#print("range of random.sample: " + str(range(0,len(all_votes))))
+		if len(all_votes) == 2:
+			rand_nums = random.sample(range(0,len(all_votes)),2)
+			msg = msg + "<@&" + str(role_id) + ">'s!!!  " + str(len(all_votes)) + " vote(s) are in and the poll is closed!  The **two** movies being passed to the council are as follows:\n```"
+		else:
+			# Change last number here to add in more movies to decision pool
+			rand_nums = random.sample(range(0,len(all_votes)),3)
+			msg = msg + "<@&" + str(role_id) + ">'s!!!  " + str(len(all_votes)) + " vote(s) are in and the poll is closed!  The **three** movies being passed to the council are as follows:\n```"
 
-		# Change last number here to add in more movies to decision pool
-		rand_nums = random.sample(range(0,len(all_votes)),2)
-
-		print("rand_nums: " + str(rand_nums))
+		# Debug
+		#print("rand_nums: " + str(rand_nums))
 
 		# Check if the random movie choices are the same (THIS IS NOT DYNAMIC RN, if you change the number of movies
 		# in the decision pool, you will need to update this
-		if all_votes[rand_nums[0]]['movie_name'] == all_votes[rand_nums[1]]['movie_name']:
-			msg = msg + "<@&" + str(role_id) + ">'s!!!  Votes are in and the poll is closed!  As a matter of fact, the same movie has been randomly chosen **twice** so it is automatically the chosen movie.  The randomly selected movie is:\n\n **" + all_votes[rand_nums[0]]['movie_name'] + "**"
-			await channel.send(msg)
-			return
+		#if all_votes[rand_nums[0]]['movie_name'] == all_votes[rand_nums[1]]['movie_name']:
+		#	msg = msg + "<@&" + str(role_id) + ">'s!!!  " + str(len(all_votes)) + " vote(s) are in and the poll is closed!  As a matter of fact, the same movie has been randomly chosen **twice** so it is automatically the chosen movie.  The randomly selected movie is:\n\n **" + all_votes[rand_nums[0]]['movie_name'] + "**"
+		#	await channel.send(msg)
+		#	return
 
-		msg = msg + "<@&" + str(role_id) + ">'s!!!  Votes are in and the poll is closed!  The movies being passed to the council are as follows:\n```"
 
 		i = 0
 		for num in rand_nums:
 			i = i + 1
-			msg = msg + "   " + str(i) + ") " + all_votes[num]['movie_name'] + " (vote cast by " + all_votes[num]['discord_name'] + ")\n"
+			msg = msg + str(i) + ") " + all_votes[num]['movie_name'].replace("_"," ") + " (vote cast by " + all_votes[num]['discord_name'] + ")\n"
 
 		msg = msg + "```"
 
-		await channel.send(msg)
+		msg_react = await channel.send(msg)
+		if len(all_votes) == 2:
+			await msg_react.add_reaction(react_emojis[0])
+			await msg_react.add_reaction(react_emojis[1])
+		else:
+			await msg_react.add_reaction(react_emojis[0])
+			await msg_react.add_reaction(react_emojis[1])
+			await msg_react.add_reaction(react_emojis[2])
+
+		# Sleep until it is movie time
+		#await asyncio.sleep(1)
+
+		one_votes   = 0
+		two_votes   = 0
+		three_votes = 0
+
+		await asyncio.sleep(5)
+
+		print(msg_react.id)
+		#for react in msg_react.reactions:
+
 		return
 
 
